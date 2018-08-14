6c32609fddf6ca6ecf6229495666bca4f7ab4108
Keith Gapusan <keithgapusan@samcd-15015s-imac.local>
2018-08-14 15:00:17 +0800

    Added the experience list of pictures in model

--- GoodWall/GoodWall/View/ViewController/ExperienceViewController/ExperienceViewController.swift
+++ GoodWall/GoodWall/View/ViewController/ExperienceViewController/ExperienceViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ExperienceViewController: UIViewController {
 
@@ -31,18 +32,21 @@ class ExperienceViewController: UIViewController {
     func initTableView(){
         cellExperienceTableViewCell = ExperienceTableViewCell()
         cellExperienceTableViewCell?.registerNib(tableView: self.tableView)
-        self.searchController = ({
-            let controller = UISearchController(searchResultsController: nil)
-            controller.searchResultsUpdater = self
-            controller.dimsBackgroundDuringPresentation = false
-            controller.searchBar.sizeToFit()
-            controller.searchBar.barStyle = UIBarStyle.black
-            controller.searchBar.barTintColor = UIColor.white
-            controller.searchBar.backgroundColor = UIColor.clear
-            self.tableView.tableHeaderView = controller.searchBar
-            return controller
-        })()
-        self.tableView.tableHeaderView = searchController.searchBar
+//        self.searchController = ({
+//            let controller = UISearchController(searchResultsController: nil)
+//            controller.searchResultsUpdater = self
+//            controller.dimsBackgroundDuringPresentation = false
+//            controller.searchBar.sizeToFit()
+//            controller.searchBar.barStyle = UIBarStyle.black
+//            controller.searchBar.barTintColor = UIColor.white
+//            controller.searchBar.backgroundColor = UIColor.clear
+//            self.tableView.tableHeaderView = controller.searchBar
+//            return controller
+//        })()
+//        self.tableView.tableHeaderView = searchController.searchBar
+        
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 607
     }
 
 }
@@ -63,10 +67,26 @@ extension ExperienceViewController: UITableViewDelegate, UITableViewDataSource{
         
         
         if let cell = tableView.dequeueReusableCell(withIdentifier: cellExperienceTableViewCell.getCellId(), for: indexPath)  as?  ExperienceTableViewCell{
+            let data = viewModel?.getDataAtRow(indexPath.row,  isActive: searchController.isActive)
+        //    let pictures  =
+            cell.lblTitle.text = data?.title ?? ""
+            cell.lblDate.text = data?.startDate ?? ""
+            cell.lblDetails.text = data?.body ?? ""
+            let listOfImages =  viewModel.getListOfPictures((data?.pictures)!)
+            print("images = \(listOfImages[0].name)  == \(String(describing: data?.pictures[0])) ")
+//            let  url = URL()
+//            if url != nil{
+//            if let url = URL(string: listOfImages[0].name!) {
+//                  cell.imgViewBanner.sd_setImage(with: url, placeholderImage: UIImage(named: "default_featured_image"))
+//            }else{
+                 cell.imgViewBanner.sd_setImage(with: URL(string: "https://placemat.imgix.net/placeholder_images/images/000/000/159/original/eDLHCtzRR0yfFtU0BQar_sylwiabartyzel_themap.jpg?ixlib=rb-1.0.0&w=250&h=&fm=auto&crop=faces%2Centropy&fit=crop&txt=250%C3%97&txtclr=BFFF&txtalign=middle%2Ccenter&txtfit=max&txtsize=42&txtfont=Avenir+Next+Demi%2CBold&bm=multiply&blend=ACACAC&s=8e9e12d71cd9edf6cebb619c99760cd7"), placeholderImage: UIImage(named: "default_featured_image"))
+//            }
             
-            cell.textLabel?.text = viewModel?.getDataAtRow(indexPath.row,  isActive: searchController.isActive).title ?? ""
+//             //   print("image \(newsList[indexPath.row].imgFeaturedUrl!)")
+//
+ //          }
             
-            
+            cell.imgUserProfile.sd_setImage(with: URL(string: "https://placem.at/people?w=500"), placeholderImage: UIImage(named: "default_featured_image"))
             return cell
         }else{
             return UITableViewCell()
